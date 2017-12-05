9a59dafb0af71ca37c9d592e6d84f9ea7f0b2d21
Meitetsu Todaka <meitetsu3@gmail.com>
2017-12-04 23:46:02 -0700

    10 leaves working fine.

--- AAE.py
+++ AAE.py
@@ -44,26 +44,22 @@ modes:
 0: Showing latest model results. InOut, true dist, discriminator, latent dist.
 """
 # you may want to change these to control your experiments
-exptitle =  'SymAE_7Lf_10002_Relu_dcrealdev3' #experiment title that goes in tensorflow folder name
+exptitle =  '10Lf_ep15_realbsdev10' #experiment title that goes in tensorflow folder name
 # AEcostToLogit
 mode= 0
 flg_graph = True # showing graphs or not during the training. Showing graphs significantly slows down the training.
-n_leaves = 7 # number of leaves in the mixed 2D Gaussian
-n_ge_epoch_booster = 1
-n_epochs_ae = 0*n_leaves # mode 1, autoencoder training epochs
+n_leaves = 10 # number of leaves in the mixed 2D Gaussian
+
 n_step_dc = 10*n_leaves # mode 2, descriminator training steps
-n_epochs_ge = 120*n_leaves # mode 3, generator training epochs
+n_epochs_ge = 15*n_leaves # mode 3, generator training epochs
 ac_batch_size = 100  # autoencoder training batch size
 import numpy as np
 blanket_resolution = 100*int(np.sqrt(n_leaves)) # blanket resoliution for descriminator or its contour plot
-dc_real_batch_size = int(blanket_resolution*blanket_resolution/3) # descriminator training real dist samplling batch size
+dc_real_batch_size = int(blanket_resolution*blanket_resolution/10) # descriminator training real dist samplling batch size
 dc_fake_batch_size = int(dc_real_batch_size/4) # generator batch size for autoencoder and fake
-keep_prob = 0.95
 
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
-from tensorflow.contrib.layers import dropout
-#from tensorflow.contrib.layers import batch_norm
 from datetime import datetime
 import os
 import matplotlib
@@ -74,24 +70,11 @@ import random
 from math import cos,sin
 from tqdm import tqdm
 
-ge_OoT_weight = 0.0001 # Out of terget weight to make reconstraction const more effective. 
-                        #Dynamically update to the same scalse as autoencoder_loss 
-#ge_batch_size = ac_batch_size*n_leaves # generator training batch size
-
-
 input_dim = 784
 xLU = [-10,10] # blanket x axis lower and upper
 yLU = [-10,10] # blanket y axis lower and upper
 n_l1 = 1000
 n_l2 = 1000
-#n_l3 = 450
-#n_l4 = 350
-#n_l5 = 350
-#n_l6 = 300
-#n_l7 = 300
-#n_l8 = 250
-#n_l9 = 250
-#n_l10 = 200
 z_dim = 2
 results_path = './Results/Adversarial_Autoencoder'
 
@@ -106,12 +89,6 @@ x_input = tf.placeholder(dtype=tf.float32, shape=[None, input_dim], name='Input'
 real_distribution = tf.placeholder(dtype=tf.float32, shape=[None, z_dim], name='Real_distribution')
 unif_distribution = tf.placeholder(dtype=tf.float32, shape=[blanket_resolution*blanket_resolution, z_dim], name='Uniform_distribution')
 is_training = tf.placeholder(tf.bool, shape=(), name='is_training')
-
-bn_params={
-        'is_training': is_training,
-        'decay': 0.99,
-        'updates_collections': None
-        }
 
 he_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 """
@@ -163,10 +140,11 @@ def show_inout(sess,op):
     plt.show()
     plt.close()
 
-def show_latent_code(sess):
+def show_latent_code(sess,spc):
     """
     Shows latent codes distribution based on all MNIST training images, with lables as color.
     Parameters. seess:TF session.
+                spc: sample per class
     No return. Displays image.
     """
     if not flg_graph:
@@ -182,8 +160,8 @@ def show_latent_code(sess):
     fig, ax = plt.subplots(1)
     
     for i in range(10):
-        y=test_zs[np.where(ytrain==i),1]
-        x=test_zs[np.where(ytrain==i),0]
+        y=test_zs[np.where(ytrain==i),1][0,0:spc]
+        x=test_zs[np.where(ytrain==i),0][0,0:spc]
         color = cm(i)
         ax.scatter(x, y, label=str(i), alpha=0.9, facecolor=color, linewidth=0.15)
         
@@ -264,22 +242,8 @@ def mlp_enc(x): # multi layer perceptron
             #weights_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01),
             #activation_fn=tf.nn.elu, 
             weights_initializer=he_init):
-        x_d = dropout(x, keep_prob, is_training=is_training)
-        elu1 = fully_connected(x_d, n_l1,scope='elu1')
-        elu1_d = dropout(elu1, keep_prob, is_training=is_training)
-        elu2 = fully_connected(elu1_d, n_l2,scope='elu2')
-#        elu2_d = dropout(elu2, keep_prob, is_training=is_training)
-#        elu3 = fully_connected(elu2_d, n_l3,scope='elu3')
-#        elu3_d = dropout(elu3, keep_prob, is_training=is_training)
-#        elu4 = fully_connected(elu3_d, n_l4,scope='elu4')
-#        elu4_d = dropout(elu4, keep_prob, is_training=is_training)
-#        elu5 = fully_connected(elu4_d, n_l5,scope='elu5')
-#        elu5_d = dropout(elu5, keep_prob, is_training=is_training)
-#        elu6 = fully_connected(elu5_d, n_l6,scope='elu6')
-#        elu7 = fully_connected(elu6, n_l7,scope='elu7')
-#        elu8 = fully_connected(elu7, n_l8,scope='elu8')
-#        elu9 = fully_connected(elu8, n_l9,scope='elu9')
-#        elu10 = fully_connected(elu9, n_l10,scope='elu10')
+        elu1 = fully_connected(x, n_l1,scope='elu1')
+        elu2 = fully_connected(elu1, n_l2,scope='elu2')
     return elu2
 
 def mlp_dec(x): # multi layer perceptron
@@ -290,22 +254,9 @@ def mlp_dec(x): # multi layer perceptron
             #weights_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01),
             #activation_fn=tf.nn.elu, 
             weights_initializer=he_init):
-#        elu10 = fully_connected(x, n_l10,scope='d_elu10')
-#        elu9 = fully_connected(elu10, n_l9,scope='d_elu9')
-#        elu8 = fully_connected(elu9, n_l8,scope='d_elu8')
-#        elu7 = fully_connected(elu8, n_l7,scope='d_elu7')
-#        elu6 = fully_connected(elu7, n_l6,scope='d_elu6') 
-#        elu5 = fully_connected(elu6, n_l5,scope='d_elu5')
-#        elu5_d = dropout(elu5, keep_prob, is_training=is_training)
-#        elu4 = fully_connected(elu5_d, n_l4,scope='d_elu4')
-#        elu14_d = dropout(elu4, keep_prob, is_training=is_training)
-#        elu3 = fully_connected(elu14_d, n_l3,scope='d_elu3')
-#        elu13_d = dropout(elu3, keep_prob, is_training=is_training)
-        elu2 = fully_connected(x, n_l2,scope='d_elu2')
-        elu12_d = dropout(elu2, keep_prob, is_training=is_training)
-        elu1 = fully_connected(elu12_d, n_l1,scope='d_elu1')
-        elu1_d = dropout(elu1, keep_prob, is_training=is_training)
-    return elu1_d
+        elu2 = fully_connected(x, n_l2,scope='elu2')
+        elu1 = fully_connected(elu2, n_l1,scope='elu1')
+    return elu1
 
 def encoder(x, reuse=False):
     """
@@ -360,7 +311,7 @@ def gaussian_mixture(batchsize, ndim, num_leaves):
     if ndim % 2 != 0:
         raise Exception("ndim must be a multiple of 2.")
     def sample(x, y, label, num_leaves):
-        shift = 1.2
+        shift = 1.7
         r = 2.0 * np.pi / float(num_leaves) * float(label)
         new_x = x * cos(r) - y * sin(r)
         new_y = x * sin(r) + y * cos(r)
@@ -441,11 +392,11 @@ with tf.name_scope("dc_loss"):
 
 with tf.name_scope("ge_loss"):
     #Out of Target penalty
-    OoT_penalty =0.0005*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_fake), logits=d_fake))
+    OoT_penalty =0.01*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_fake), logits=d_fake))
     dens = 0.0001*density(encoder_output,0.1) # density. This dodes not work to spread codes. dc should work.
     dist_to_target = 0.001*tf.reduce_max(cdist(encoder_output, real_distribution,agg='min'))
         #it tend to become thin line compared to OoT only, since it looks at the original density.
-    generator_loss = autoencoder_loss+OoT_penalty+dist_to_target #not sure why it averages out
+    generator_loss = autoencoder_loss+OoT_penalty #not sure why it averages out
 
 #optimizer
 all_variables = tf.trainable_variables()
@@ -491,15 +442,15 @@ def tb_init(sess): # create tb path, model path and return tb writer and saved m
     writer = tf.summary.FileWriter(logdir=tensorboard_path, graph=sess.graph)  
     return writer, saved_model_path
 
-def tb_write(sess,n_batches):
+def tb_write(sess):
     batch_x, _ = mnist.train.next_batch(dc_fake_batch_size)
     idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
     z_real_batch = z_real_dist[idx,:]
     dc_loss_v,ge_loss_v,ae_loss_v,OoT_pen_v,dtt_v,dc_summary,ge_summary,ae_summary,oot_summary,den_summary,dtt_summary \
         = sess.run([dc_loss,generator_loss,autoencoder_loss,OoT_penalty,dist_to_target,dc_sm, ge_sm, ae_sm,oot_sm,den_sm,dtt_sm] \
         ,feed_dict={is_training:False, x_input: batch_x,real_distribution:z_real_batch,unif_distribution:blanket})
-    tqdm.write("Batch Per Epoch: {} , Generator Loss:{},Descriminator Loss:{}".format(n_batches,ge_loss_v,dc_loss_v))
-    tqdm.write("dc_loss:{},ae_loss:{},OoT_pen:{},DTT:{}".format(dc_loss_v,ae_loss_v,OoT_pen_v,dtt_v))
+    tqdm.write("ae_loss:{1:.5f},dc_loss:{0:.5f},Generator Loss:{1:.5f},OoT_pen:{2:.5f},DTT:{3:.5f}"\
+               .format(ae_loss_v,dc_loss_v,ge_loss_v,OoT_pen_v,dtt_v))
     writer.add_summary(ae_summary, global_step=step)
     writer.add_summary(dc_summary, global_step=step)
     writer.add_summary(ge_summary, global_step=step)
@@ -514,81 +465,46 @@ with tf.Session() as sess:
         z_real_dist = gaussian_mixture(55000, z_dim, n_leaves)
         idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
         z_real_batch = z_real_dist[idx,:]
-    if mode==1 or mode==-1: # Autoencoder training mode
-        print("-------1: Autoencoder Training Mode--------")
-        # showing In and Out Graphs, and latent code before training
-        show_inout(sess, op=decoder_output)  
-        show_latent_code(sess)        
-        for i in range(n_epochs_ae):
-            n_batches = int(mnist.train.num_examples / ac_batch_size)
-            print("------------------Epoch {}/{}------------------".format(i, n_epochs_ae))
-            for b in tqdm(range(n_batches)):
-                batch_x, _ = mnist.train.next_batch(ac_batch_size)
-                sess.run(autoencoder_optimizer, feed_dict={is_training:True,x_input: batch_x})
-                if b % 50 == 0:
-                    loss,summary = sess.run([autoencoder_loss, ae_sm],
-                                            feed_dict={is_training:False, x_input: batch_x})
-                    writer.add_summary(summary, global_step=step)
-                    tqdm.write("Batch {} / {}, Autoencoder Loss:{}".format(b,n_batches,loss))
-                step += 1
-            # showing graphs afer the training of an epoch
-            if i % 5 == 0:
-                show_inout(sess, op=decoder_output)  
-                show_latent_code(sess)
-        saver.save(sess, save_path=saved_model_path, global_step=step)
     if mode==2 or mode==-1: # Descriminator training mode
-        print("-------2: Descriminator Training Mode--------")    
-        #restoring the result from the privious mode
-        save_metagraph = model_restore(saver,mode)
-        # showing true distribution and descriminator contour  before training
+        print("-------Descriminator Pre-Training--------")    
+        show_inout(sess, op=decoder_output)  
+        show_latent_code(sess,300)     
         show_real_dist(z_real_dist)
         show_discriminator(sess)
-        
         for i in tqdm(range(n_step_dc)):
             batch_x, _ = mnist.train.next_batch(dc_fake_batch_size)
             sess.run(discriminator_optimizer, feed_dict={is_training:True ,x_input: batch_x,real_distribution:z_real_batch,unif_distribution:blanket })
             if i % 10 == 0:
-                loss,summary = sess.run([dc_loss, dc_sm],
+                dc_loss_v,dc_summary = sess.run([dc_loss, dc_sm],
                                             feed_dict={is_training:False, x_input: batch_x,unif_distribution:blanket,real_distribution:z_real_batch})
-                writer.add_summary(summary, global_step=step)
+                writer.add_summary(dc_summary, global_step=step)
                 show_discriminator(sess)
-                tqdm.write("Batch {} / {}, Descriminator Loss:{}".format(i,n_step_dc,loss)) 
+                tqdm.write("Batch {} / {}, Descriminator Loss:{}".format(i,n_step_dc,dc_loss_v)) 
             step += 1
         show_discriminator(sess)
-        saver.save(sess, save_path=saved_model_path, global_step=step, write_meta_graph = save_metagraph)
+        saver.save(sess, save_path=saved_model_path, global_step=step, write_meta_graph = True)
     if mode==3 or mode==-1: # Latent regulation
-        print("-------3: Latent Training Mode--------")
         #restoring the result from the privious mode
         save_metagraph = model_restore(saver,mode)
-        # showing descriminator contour and latent code before training
-        show_discriminator(sess)
-        show_latent_code(sess)   
+        n_batches = int(mnist.train.num_examples / ac_batch_size)
         for i in range(n_epochs_ge):
-            print("------------------Epoch {}/{} Descriminator------------------".format(i, n_epochs_ge))
-            n_batches = int(mnist.train.num_examples / dc_fake_batch_size)
-            tb_write(sess,n_batches)
-            for b in tqdm(range(n_batches)):
-                batch_x, _ = mnist.train.next_batch(dc_fake_batch_size)
+            print("------------------Epoch {}/{} ------------------".format(i, n_epochs_ge))
+            for b in tqdm(range(n_batches)):    
+                #Autoencoder
+                batch_x, _ = mnist.train.next_batch(ac_batch_size)
+                #Discriminator
                 idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
                 z_real_batch = z_real_dist[idx,:]
                 sess.run([discriminator_optimizer],feed_dict=\
                  {is_training:True, x_input: batch_x, real_distribution:z_real_batch,unif_distribution:blanket })
-                step += 1
-            show_discriminator(sess)
-            show_latent_code(sess)
-            
-            print("------------------Epoch {}/{} Generator------------------".format(i, n_epochs_ge))
-            tb_write(sess,n_batches)
-            for b in tqdm(range(n_batches*n_ge_epoch_booster)):
-                batch_x, _ = mnist.train.next_batch(dc_fake_batch_size)
-                idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
-                z_real_batch = z_real_dist[idx,:]
+                #Generator
                 sess.run([generator_optimizer],feed_dict=\
-                 {is_training:True, x_input: batch_x, real_distribution:z_real_batch,unif_distribution:blanket })
+                 {is_training:True, x_input: batch_x})
+                if b % 10 == 0:
+                    show_discriminator(sess)
+                    show_latent_code(sess,300)
+                    tb_write(sess)
                 step += 1
-            # showing graphs afer the training of an epoch
-            show_discriminator(sess)
-            show_latent_code(sess)
         saver.save(sess, save_path=saved_model_path, global_step=step, write_meta_graph = save_metagraph)
     if mode != 0: # some kind of training
         writer.close()
@@ -598,6 +514,6 @@ with tf.Session() as sess:
         z_real_dist = gaussian_mixture(55000, z_dim, n_leaves)
         show_real_dist(z_real_dist)
         show_discriminator(sess)    
-        show_latent_code(sess)
+        show_latent_code(sess,300)
     
         
