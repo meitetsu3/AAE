140f4cfe80218d8e76e339034395daa2b10b5977
Meitetsu Todaka <meitetsu3@gmail.com>
2017-12-11 18:25:13 -0700

    mid point of major enhancement to get supervised learning

--- AAE.py
+++ AAE.py
@@ -34,36 +34,37 @@ modes:
 0: Showing latest model results. InOut, true dist, discriminator, latent dist.
 """
 # you may want to change these to control your experiments
-exptitle =  '10Lf' #experiment title that goes in tensorflow folder name
-
-moderestore = '20171210131913_2_10Lf_realbs40'
-mode= 0
+exptitle =  '10Lf_sup_ref2' #experiment title that goes in tensorflow folder name
+moderestore = ''
+mode= 1
 flg_graph = True # showing graphs or not during the training. Showing graphs significantly slows down the training.
 n_leaves = 10 # number of leaves in the mixed 2D Gaussian
 OoTWeight = 0.01 # out of target weight in generator
 DtTWeight = 0.001 # distance to target weight
 n_latent_sample = 5000 # latent code visualization sample
 n_step_dc = 0*n_leaves # mode 2, descriminator training steps
-n_epochs_ge = 18*n_leaves # mode 3, generator training epochs
-ac_batch_size = 100  # autoencoder training batch size
+n_epochs_ge = 45*n_leaves-400 # mode 3, generator training epochs
+ac_batch_size = 160  # autoencoder training batch size
 tb_batch_size = 800  # x_inputs batch size for tb
 tb_log_step = 200 # tb logging step
 flg_console_log = False # console logging swtich
 import numpy as np
 blanket_resolution = 10*int(np.sqrt(n_leaves)) # blanket resoliution for descriminator or its contour plot
 dc_real_batch_size = int(blanket_resolution*blanket_resolution/40) # descriminator training real dist samplling batch size
+dc_contour_res_x = 5 # x to the blanket resolution
 
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 from datetime import datetime
 import os
 import matplotlib
-#matplotlib.use('GTKAgg') # to speed up graphing
 import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 import random
 from math import cos,sin
 from tqdm import tqdm
+
+myColor = ['black','orange', 'red', 'blue','gray','green','pink','cyan','Purple','lime','magenta']
 
 input_dim = 784
 xLU = [-10,10] # blanket x axis lower and upper
@@ -82,8 +83,10 @@ mnist = input_data.read_data_sets('./Data', one_hot=True)
 # Placeholders for input data and the targets
 x_input = tf.placeholder(dtype=tf.float32, shape=[None, input_dim], name='Input')
 real_distribution = tf.placeholder(dtype=tf.float32, shape=[None, z_dim], name='Real_distribution')
-unif_distribution = tf.placeholder(dtype=tf.float32, shape=[blanket_resolution*blanket_resolution, z_dim], name='Uniform_distribution')
-is_training = tf.placeholder(tf.bool, shape=(), name='is_training')
+real_lbl = tf.placeholder(dtype=tf.float32, shape=[None,1],name = 'Real_lbl')
+fake_lbl = tf.placeholder(dtype=tf.float32, shape=[None,1],name = 'Fake_lbl')
+unif_d = tf.placeholder(dtype=tf.float32, shape=[None,1],name = 'Uniform_d')
+unif_z = tf.placeholder(dtype=tf.float32, shape=[blanket_resolution*blanket_resolution, z_dim], name='Uniform_z')
 
 he_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 """
@@ -94,8 +97,7 @@ def form_results():
     Forms folders for each run to store the tensorboard files, saved models and the log files.
     :return: three string pointing to tensorboard, saved models and log paths respectively.
     """
-    folder_name = "/{0}_{1}_{2}". \
-        format(datetime.now().strftime("%Y%m%d%H%M%S"), mode,exptitle)
+    folder_name = "/{0}_{1}_{2}".format(datetime.now().strftime("%Y%m%d%H%M%S"), mode,exptitle)
     tensorboard_path = results_path + folder_name + '/Tensorboard'
     saved_model_path = results_path + folder_name + '/Saved_models/'
     log_path = results_path + folder_name + '/log'
@@ -117,7 +119,7 @@ def show_inout(sess,op):
         return
     idx = random.sample(range(mnist[0].num_examples),ac_batch_size)
     img_in = mnist[0].images[idx,:]
-    img_out = sess.run(op, feed_dict={x_input: img_in, is_training:False})
+    img_out = sess.run(op, feed_dict={x_input: img_in})
     img_out_s = img_out.reshape(ac_batch_size,28,28)
     img_in_s = img_in.reshape(ac_batch_size,28,28)
     #.reshape(10,28,28)
@@ -144,23 +146,24 @@ def show_latent_code(sess,spc):
     """
     if not flg_graph:
         return
-    plt.rc('figure', figsize=(5, 5))
+    plt.rc('figure', figsize=(10, 10))
     plt.tight_layout()
     
     with tf.variable_scope("Encoder"):
-        test_zs = sess.run(encoder(mnist[0].images, reuse=True),feed_dict={is_training:False})
+        test_zs = sess.run(encoder(mnist[0].images, reuse=True))
     ytrain = np.argmax(mnist[0].labels,axis=1)
     
-    cm = plt.cm.get_cmap('tab10')
+    cm = matplotlib.colors.ListedColormap(myColor[1:])
+    
     fig, ax = plt.subplots(1)
     
     for i in range(10):
         y=test_zs[np.where(ytrain==i),1][0,0:spc]
         x=test_zs[np.where(ytrain==i),0][0,0:spc]
         color = cm(i)
-        ax.scatter(x, y, label=str(i), alpha=0.9, facecolor=color, linewidth=0.02, s = 2)
+        ax.scatter(x, y, label=str(i), alpha=0.9, facecolor=color, linewidth=0.02, s = 10)
     
-    ax.legend(loc='center left', markerscale = 8, bbox_to_anchor=(1, 0.5))
+    ax.legend(loc='center left', markerscale = 3, bbox_to_anchor=(1, 0.5))
     ax.set_title('2D latent code')    
     plt.show()
     plt.close()
@@ -173,7 +176,8 @@ def show_discriminator(sess):
     """
     if not flg_graph:
         return
-    xlist, ylist, blanket = get_blanket(blanket_resolution)
+    br = dc_contour_res_x*blanket_resolution
+    xlist, ylist, blanket = get_blanket(br)
     
     plt.rc('figure', figsize=(6, 5))
     plt.tight_layout()
@@ -181,12 +185,12 @@ def show_discriminator(sess):
     X, Y = np.meshgrid(xlist, ylist)    
     
     with tf.variable_scope("Discriminator"):
-        desc_result = sess.run(tf.nn.sigmoid(discriminator(blanket, reuse=True)),feed_dict={is_training:False})
+        desc_result = sess.run(tf.nn.sigmoid(discriminator(blanket, reuse=True)))
 
-    Z = np.empty((blanket_resolution,blanket_resolution), dtype="float32")    
-    for i in range(blanket_resolution):
-        for j in range(blanket_resolution):
-            Z[i][j]=desc_result[i*blanket_resolution+j]
+    Z = np.empty((br,br), dtype="float32")    
+    for i in range(br):
+        for j in range(br):
+            Z[i][j]=desc_result[i*br+j]
 
     fig, ax = plt.subplots(1)
     cp = ax.contourf(X, Y, Z)
@@ -206,7 +210,7 @@ def get_blanket(resolution):
             blanket[i*resolution+j]=[xlist[j],ylist[i]]
     return xlist,ylist,blanket
             
-def show_real_dist(z_real_dist):
+def show_real_dist(z_real_dist, real_lbl_ins):
     """
     Shows real distribution
     Parameters. z_real_dist:(batch_size,2) numpy array
@@ -214,17 +218,25 @@ def show_real_dist(z_real_dist):
     """
     if not flg_graph:
         return
-    plt.rc('figure', figsize=(5, 5))
+    plt.rc('figure', figsize=(10, 10))
     plt.tight_layout()
     fig, ax = plt.subplots(1)
-    ax.scatter(z_real_dist[:,0], z_real_dist[:,1], alpha=0.9, linewidth=0.15)
+    cm = matplotlib.colors.ListedColormap(myColor)
 
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    ax.set_title('Real Distribution')   
+    for i in range(-1,10):
+        y=z_real_dist[np.where(real_lbl_ins==i),1]
+        x=z_real_dist[np.where(real_lbl_ins==i),0]
+        color = cm(i+1)
+        ax.scatter(x,y,label=str(i), alpha=0.9, facecolor=color, linewidth=0.15,s = 5)
+
+    ax.legend(loc='center left',  markerscale = 5, bbox_to_anchor=(1, 0.5))
+    ax.set_title('Real Distribution')
+    
     plt.xlim(xLU[0],xLU[1])
     plt.ylim(yLU[0],yLU[1])
     plt.show()
     plt.close()
+
 
 def leaky_relu(z, name=None):
     return tf.maximum(0.01 * z, z, name=name)
@@ -266,7 +278,7 @@ def encoder(x, reuse=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
     last_layer = mlp_enc(x)
-    output = fully_connected(last_layer, z_dim,weights_initializer=he_init, scope='None7',activation_fn=None)
+    output = fully_connected(last_layer, z_dim,weights_initializer=he_init, scope='None',activation_fn=None)
     return output
 
 def decoder(x, reuse=False):
@@ -280,31 +292,31 @@ def decoder(x, reuse=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
     last_layer = mlp_dec(x)
-    output = fully_connected(last_layer, input_dim, weights_initializer=he_init, scope='Sigmoid7', activation_fn=tf.sigmoid)
+    output = fully_connected(last_layer, input_dim, weights_initializer=he_init, scope='Sigmoid', activation_fn=tf.sigmoid)
     return output
 
-def discriminator(x, reuse=False):
+def discriminator(x, dg, reuse=False):
     """
     Discriminator that leanes to activate at true distribution and not for the others.
     :param x: tensor of shape [batch_size, z_dim]
+    :param dg: one-hot-vector as a selector of a leaf.
     :param reuse: True -> Reuse the discriminator variables, False -> Create the variables
     :return: tensor of shape [batch_size, 1]. I think it's better to take sigmoid here.
     """
     if reuse:
         tf.get_variable_scope().reuse_variables()
-    last_layer = mlp_dec(x)
-    output = fully_connected(last_layer, 1, weights_initializer=he_init, scope='None7',activation_fn=None)
+    last_layer = mlp_dec(tf.concat([x,dg],1))
+    output = fully_connected(last_layer, 1, weights_initializer=he_init, scope='None',activation_fn=None)
     return output
 
-def gaussian_mixture(batchsize, ndim, num_leaves):
+def gaussian_mixture(batchsize, num_leaves, sel):
     """
     Crate true distribution with num_leaves 2D Gaussian
     :batch_size: number of data points to generate
-    :ndim: latet code dimention =2
+    :sel: selector. 0 means all leaves.
     :return: tensor of shape [batch_size, 2]. I think it's better to take sigmoid here.
     """
-    if ndim % 2 != 0:
-        raise Exception("ndim must be a multiple of 2.")
+
     def sample(x, y, label, num_leaves):
         shift = 1.7
         r = 2.0 * np.pi / float(num_leaves) * float(label)
@@ -316,12 +328,13 @@ def gaussian_mixture(batchsize, ndim, num_leaves):
 
     x_var = 0.5
     y_var = 0.07
-    x = np.random.normal(0, x_var, (batchsize, ndim // 2))
-    y = np.random.normal(0, y_var, (batchsize, ndim // 2))
-    z = np.empty((batchsize, ndim), dtype=np.float32)
+    x = np.random.normal(0, x_var, (batchsize, 2 // 2))
+    y = np.random.normal(0, y_var, (batchsize, 2 // 2))
+    z = np.empty((batchsize, 2), dtype=np.float32)
     for batch in range(batchsize):
-        for zi in range(ndim // 2):
-            z[batch, zi*2:zi*2+2] = sample(x[batch, zi], y[batch, zi], random.randint(0, num_leaves - 1), num_leaves)
+        for zi in range(2 // 2):
+            s = np.random.randint(0, num_leaves) if sel[batch] == -1 else sel[batch]
+            z[batch, zi*2:zi*2+2] = sample(x[batch, zi], y[batch, zi], s, num_leaves)
     return z
 
 def model_restore(saver,pmode,mname=''):
@@ -358,9 +371,9 @@ with tf.variable_scope('Decoder'):
     decoder_output = decoder(encoder_output)
 
 with tf.variable_scope('Discriminator'):
-    d_real = discriminator(real_distribution)
-    d_blanket = discriminator(unif_distribution, reuse=True)
-    d_fake = discriminator(encoder_output, reuse=True)
+    d_real = discriminator(real_distribution,real_lbl)
+    d_blanket = discriminator(unif_z, unif_d, reuse=True)
+    d_fake = discriminator(encoder_output,fake_lbl, reuse=True)
     
 # loss
 with tf.name_scope("ae_loss"):
@@ -375,7 +388,7 @@ with tf.name_scope("dc_loss"):
 with tf.name_scope("ge_loss"):
     #Out of Target penalty
     OoT_penalty =OoTWeight*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_fake), logits=d_fake))
-    dist_to_target = DtTWeight*tf.reduce_max(cdist(encoder_output, real_distribution,agg='min'))
+    #dist_to_target = DtTWeight*tf.reduce_max(cdist(encoder_output, real_distribution,agg='min'))
     generator_loss = autoencoder_loss+OoT_penalty #not sure why it averages out
 
 #optimizer
@@ -403,7 +416,7 @@ ae_sm = tf.summary.scalar(name='Autoencoder_Loss', tensor=autoencoder_loss)
 dc_sm = tf.summary.scalar(name='Discriminator_Loss', tensor=dc_loss)
 ge_sm = tf.summary.scalar(name='Generator_Loss', tensor=generator_loss)
 oot_sm = tf.summary.scalar(name='OoT_penalty', tensor=OoT_penalty)
-dtt_sm = tf.summary.scalar(name='dist_to_target', tensor=dist_to_target)
+#dtt_sm = tf.summary.scalar(name='dist_to_target', tensor=dist_to_target)
 summary_op = tf.summary.merge_all()
 
 # Creating saver and get ready
@@ -431,28 +444,29 @@ def tb_write(sess):
 #        tqdm.write("ae_loss:{0:.5f},dc_loss:{1:.5f},Generator Loss:{2:.5f},OoT_pen:{3:.5f},DTT:{4:.5f}"\
 #               .format(ae_loss_v,dc_loss_v,ge_loss_v,OoT_pen_v,dtt_v))
 #    else:
-    sm = sess.run(summary_op,feed_dict={is_training:False, x_input: batch_x,real_distribution:z_real_batch,unif_distribution:blanket})
+    sm = sess.run(summary_op,feed_dict={x_input: batch_x,real_distribution:z_real_batch,unif_d:blanket})
     writer.add_summary(sm, global_step=step)
 
 with tf.Session() as sess:
     if mode != 0: # some kind of training
         writer,saved_model_path = tb_init(sess)   
         _,_,blanket = get_blanket(blanket_resolution)
-        z_real_dist = gaussian_mixture(55000, z_dim, n_leaves)
+        real_lbl_ins = np.random.randint(-1,10, size=5000)
+        z_real_dist = gaussian_mixture(5000, n_leaves,real_lbl_ins)
     if mode==1: # Descriminator training mode
         print("-------Descriminator Training--------")    
         show_inout(sess, op=decoder_output)  
         show_latent_code(sess,n_latent_sample)     
-        show_real_dist(z_real_dist)
+        show_real_dist(z_real_dist,real_lbl_ins)
         show_discriminator(sess)
         batch_x, _ = mnist.train.next_batch(0)
         for i in tqdm(range(n_step_dc)):
             idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
             z_real_batch = z_real_dist[idx,:]
-            sess.run(discriminator_optimizer, feed_dict={is_training:True ,x_input: batch_x,real_distribution:z_real_batch,unif_distribution:blanket })
+            sess.run(discriminator_optimizer, feed_dict={x_input: batch_x,real_distribution:z_real_batch,unif_d:blanket })
             if i % tb_log_step == 0:
                 dc_loss_v,dc_summary = sess.run([dc_loss, dc_sm],
-                                            feed_dict={is_training:False, x_input: batch_x,unif_distribution:blanket,real_distribution:z_real_batch})
+                                            feed_dict={x_input: batch_x,unif_d:blanket,real_distribution:z_real_batch})
                 writer.add_summary(dc_summary, global_step=step)
                 show_discriminator(sess)
                 tqdm.write("Batch {} / {}, Descriminator Loss:{}".format(i,n_step_dc,dc_loss_v)) 
@@ -465,16 +479,13 @@ with tf.Session() as sess:
             print("------------------Epoch {}/{} ------------------".format(i, n_epochs_ge))
             for b in tqdm(range(n_batches)):    
                 #Autoencoder
-                    
                 #Discriminator
                 batch_x, _ = mnist.train.next_batch(ac_batch_size)    
                 idx = random.sample(range(mnist[0].num_examples),dc_real_batch_size)
                 z_real_batch = z_real_dist[idx,:]
-                sess.run([discriminator_optimizer],feed_dict=\
-                 {is_training:True, x_input: batch_x, real_distribution:z_real_batch,unif_distribution:blanket })
+                sess.run([discriminator_optimizer],feed_dict={x_input: batch_x, real_distribution:z_real_batch,unif_d:blanket })
                 #Generator
-                sess.run([generator_optimizer],feed_dict=\
-                 {is_training:True, x_input: batch_x,real_distribution:z_real_batch})
+                sess.run([generator_optimizer],feed_dict={x_input: batch_x})
                 if b % tb_log_step == 0:
                     show_discriminator(sess)
                     show_latent_code(sess,n_latent_sample)
@@ -486,8 +497,8 @@ with tf.Session() as sess:
     if mode==0: # showing the latest model result. InOut, true dist, discriminator, latent dist.
         model_restore(saver,mode,moderestore)
         show_inout(sess, op=decoder_output) 
-        z_real_dist = gaussian_mixture(55000, z_dim, n_leaves)
-        show_real_dist(z_real_dist)
+        z_real_dist = gaussian_mixture(5000, n_leaves,)
+        show_real_dist(z_real_dist,real_lbl_ins)
         show_discriminator(sess)    
         show_latent_code(sess,n_latent_sample)
     
