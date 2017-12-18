8161f789a8888701b544c899f91dc7c903b0568e
Meitetsu Todaka <meitetsu3@gmail.com>
2017-12-18 00:36:36 -0700

    validating if this is hitting 95%+ accuracy with 1000 MNIST labels with semi-supervised learning. Nice to hit 98%+

--- semi-supervised_flower.py
+++ semi-supervised_flower.py
@@ -3,9 +3,8 @@
 Created on Thu Nov 16 11:50:32 2017
 ref: https://github.com/Naresh1318/Adversarial_Autoencoder
 @author: mtodaka
-: y semi-supervised training
-: accuracy measurement
-: cat descriminator
+: categorical descriminator
+: separate? reconstruction, descriminator, generater, semi-supervised
 : learning rate schedule
 :feed mode to run ./file
 """
@@ -15,20 +14,22 @@ modes:
 1: Latent regulation. train generator to fool Descriminator with reconstruction constraint.
 0: Showing latest model results. InOut, true dist, discriminator, latent dist.
 """
-exptitle =  '10Lf_classification' #experiment title that goes in tensorflow folder name
+exptitle =  '10Lf_TestAccuracy_SemiSbatch1000' #experiment title that goes in tensorflow folder name
 mode = 1
 flg_graph = False # showing graphs or not during the training. Showing graphs significantly slows down the training.
 model_folder = '' # name of the model to be restored. white space means most recent.
 n_leaves = 10 # number of leaves in the mixed 2D Gaussian
-n_epochs_ge = 5*n_leaves # mode 3, generator training epochs
+n_epochs_ge = 150*n_leaves # mode 3, generator training epochs
 ac_batch_size = 500  # autoencoder training batch size
+semi_sup_batch_size = 1000 # semi-supervised training batch size
 import numpy as np
 blanket_resolution = 10*int(np.sqrt(n_leaves)) # blanket resoliution for descriminator or its contour plot
 dc_real_batch_size = int(blanket_resolution*blanket_resolution/15) # descriminator training real dist samplling batch size
 n_label = 1000 # number of labels used in semi-supervised training
 
 OoTWeight = 0.01 # out of target weight in generator
-ClassificationWeight = 0.01 #classification weight in generator
+ClassificationWeight = 0.1 #classification weight in generator
+Yreg_weight = 0.01 # Y regulation or distance to vertex weight
 n_latent_sample = 5000 # latent code visualization sample
 tb_batch_size = 800  # x_inputs batch size for tb
 tb_log_step = 200  # tb logging step
@@ -111,7 +112,10 @@ def model_restore(saver,pmode,mname=''):
         all_results = [path for path in os.listdir(results_path) if '_'+str(pmode-1)+'_' in path or '_-1_' in path] 
     all_results.sort()
     saver.restore(sess, save_path=tf.train.latest_checkpoint(results_path + '/' + all_results[-1] + '/Saved_models/'))
-          
+
+def next_batch(x, y, batch_size):
+    random_index = np.random.permutation(np.arange(len(x)))[:batch_size]
+    return x[random_index], y[random_index]         
 """
 Vis Functions
 """
@@ -268,7 +272,8 @@ def encoder(x, reuse=False):
         tf.get_variable_scope().reuse_variables()
     last_layer = mlp_enc(x)
     outputZ = fully_connected(last_layer, z_dim,weights_initializer=he_init, scope='linZ',activation_fn=None)
-    outputY = fully_connected(last_layer, n_leaves, weights_initializer=he_init, scope='linY',activation_fn=None)
+    outputY = fully_connected(last_layer, n_leaves, weights_initializer=he_init,\
+                              scope='linY',activation_fn=None)
     
     return outputZ,outputY
 
@@ -284,7 +289,7 @@ def decoder(z,y, reuse=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
     last_layer = mlp_dec(x)
-    output = fully_connected(last_layer, input_dim, weights_initializer=he_init, scope='Sigmoid', activation_fn=tf.sigmoid)
+    output = fully_connected(last_layer, input_dim, weights_initializer=he_init,scope='Sigmoid', activation_fn=tf.sigmoid)
     return output
 
 def discriminator(x, dg, reuse=False):
@@ -324,8 +329,9 @@ def gaussian_mixture(batchsize, num_leaves, sel):
     z = np.empty((batchsize, 2), dtype=np.float32)
     for batch in range(batchsize):
         for zi in range(2 // 2):
-            s = np.where(sel[batch]==1)[0][0]
-            z[batch, zi*2:zi*2+2] = sample(x[batch, zi], y[batch, zi], s, num_leaves)
+            #s = np.where(sel[batch]==1)[0][0]
+            z[batch, zi*2:zi*2+2] = np.array([x[batch, zi], y[batch, zi]]).reshape((2,))
+            #sample(x[batch, zi], y[batch, zi], s, num_leaves)
     return z
 
  
@@ -333,18 +339,24 @@ def gaussian_mixture(batchsize, num_leaves, sel):
 Defining key operations, Loess, Optimizer and other necessary operations
 """
 with tf.variable_scope('Encoder'):
-    encoder_outputZ,encoder_outputY = encoder(x_input)
-    _,trainer_y = encoder(trainer_input, reuse=True)
+    encoder_outputZ,encoder_outputYlogits = encoder(x_input)
+    _,trainer_ylogits = encoder(trainer_input, reuse=True)
     
 with tf.variable_scope('Decoder'):
-    decoder_output = decoder(encoder_outputZ,tf.one_hot(tf.argmax(encoder_outputY, dimension = 1), depth = n_leaves))
+    decoder_output = decoder(encoder_outputZ,tf.one_hot(tf.argmax(encoder_outputYlogits, dimension = 1), depth = n_leaves))
     
 with tf.variable_scope('Discriminator'):
     d_real = discriminator(real_distribution,real_lbl)
     d_blanket = discriminator(unif_z, unif_d, reuse=True)
-    d_fake = discriminator(encoder_outputZ,tf.one_hot(tf.argmax(encoder_outputY, dimension = 1), depth = n_leaves), reuse=True)
+    d_fake = discriminator(encoder_outputZ,tf.one_hot(tf.argmax(encoder_outputYlogits, dimension = 1), depth = n_leaves), reuse=True)
     
 # loss 
+with tf.name_scope('Y_regulation'):
+    with tf.name_scope('unsupervised_simplex_distToVertex'):
+        Yonehot = tf.one_hot(tf.argmax(encoder_outputYlogits, dimension = 1), depth = n_leaves)
+        Ysoftmax = tf.nn.softmax(encoder_outputYlogits)
+        dist_to_vertex = Yreg_weight*tf.reduce_mean(tf.reduce_sum(tf.square( Yonehot- encoder_outputYlogits),axis=1))
+
 with tf.name_scope("dc_loss"):
     dc_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_real), logits=d_real))
     dc_loss_blanket = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(d_blanket), logits=d_blanket))
@@ -353,28 +365,34 @@ with tf.name_scope("dc_loss"):
 
 with tf.name_scope("ge_loss"):
     autoencoder_loss = tf.reduce_mean(tf.square(x_input - decoder_output))
-    classification_loss = ClassificationWeight*tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=trainer_lbl,logits=trainer_y, name="classification_loss"))
+    classification_loss = ClassificationWeight*tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=trainer_lbl,logits=trainer_ylogits, name="classification_loss"))
     #Out of Target penaltyreal_lbl
     OoT_penalty =OoTWeight*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_fake), logits=d_fake))
-    generator_loss = autoencoder_loss+OoT_penalty+classification_loss #not sure why it averages out
+    generator_loss = autoencoder_loss+classification_loss+OoT_penalty+dist_to_vertex #not sure why it averages out
 
+# metrics
 with tf.name_scope('accuracy'):
     with tf.name_scope('train_accuracy'):
-        t_correct_prediction = tf.equal(tf.argmax(trainer_lbl, 1), tf.argmax(trainer_y, 1))
+        t_correct_prediction = tf.equal(tf.argmax(trainer_lbl, 1), tf.argmax(trainer_ylogits, 1))
         t_accuracy = tf.reduce_mean(tf.cast(t_correct_prediction, tf.float32))
     with tf.name_scope('accuracy'):
-        correct_prediction = tf.equal(tf.argmax(fake_lbl, 1), tf.argmax(encoder_outputY, 1))
+        correct_prediction = tf.equal(tf.argmax(fake_lbl, 1), tf.argmax(encoder_outputYlogits, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 #optimizer
 all_variables = tf.trainable_variables()
 dc_var = [var for var in all_variables if 'Discriminator/' in var.name]
 ae_var = [var for var in all_variables if ('Encoder/' in var.name or 'Decoder/' in var.name)]
+a_var = [var for var in all_variables if ('Encoder/' in var.name)]
 
 with tf.name_scope("AE_optimizer"):
     autoencoder_optimizer = tf.train.AdamOptimizer().minimize(autoencoder_loss)
 
 with tf.name_scope("DC_optimizer"):
     discriminator_optimizer = tf.train.AdamOptimizer().minimize(dc_loss, var_list=dc_var)
+
+with tf.name_scope("Y_optimizer"):
+    Y_optimizer = tf.train.AdamOptimizer().minimize(dist_to_vertex, var_list=a_var)    
     
 with tf.name_scope("GE_optimizer"):
     generator_optimizer = tf.train.AdamOptimizer().minimize(generator_loss, var_list=ae_var)
@@ -393,6 +411,7 @@ oot_sm = tf.summary.scalar(name='OoT_penalty', tensor=OoT_penalty)
 cll_sm = tf.summary.scalar(name='Classification_Loss', tensor=classification_loss)
 acc_sm = tf.summary.scalar(name='Accuracy', tensor=accuracy)
 tac_sm = tf.summary.scalar(name='Training_Accuracy', tensor=t_accuracy)
+dtv_sm = tf.summary.scalar(name='Distance_to_Vertex', tensor=dist_to_vertex)
 summary_op = tf.summary.merge_all()
 
 # Creating saver and get ready
@@ -410,17 +429,11 @@ def tb_init(sess): # create tb path, model path and return tb writer and saved m
     return writer, saved_model_path
 
 def tb_write(sess):
-    batch_x, batch_y = mnist.train.next_batch(tb_batch_size)
-    #batch_y_fl = batch_y.astype(float)
-    
-    # increase batch size for tb to show stable stats and extend freq?            
-    dc_real_lbl = np.eye(10)[np.array(np.random.randint(0,n_leaves, size=dc_real_batch_size)).reshape(-1)]
-    dc_real_dist = gaussian_mixture(dc_real_batch_size, n_leaves,dc_real_lbl)
-                
-    dc_blanket_digit = np.eye(10)[np.array(np.random.randint(0,n_leaves, size=blanket_resolution*blanket_resolution)).reshape(-1)]
-                
-    sm = sess.run(summary_op,feed_dict={x_input: batch_x, real_distribution:dc_real_dist,\
-             real_lbl:dc_real_lbl ,unif_z:blanket, unif_d:dc_blanket_digit, trainer_input:trainer_x,trainer_lbl:trainer_y,fake_lbl:batch_y})
+    batch_x, batch_y = mnist.train.next_batch(tb_batch_size) #bigger batch to see ae_loss with stability
+    test_x, test_y = mnist.test.next_batch(1000) # to evalute accuracy with unseen data.
+    # use the priviousely generated data for others
+    sm = sess.run(summary_op,feed_dict={x_input: batch_x, real_distribution:dc_real_z,\
+             real_lbl:dc_real_lbl ,unif_z:blanket, unif_d:dc_blanket_digit, trainer_input:test_x,trainer_lbl:test_y,fake_lbl:batch_y})
     writer.add_summary(sm, global_step=step)
 
 with tf.Session() as sess:
@@ -428,25 +441,25 @@ with tf.Session() as sess:
         writer,saved_model_path = tb_init(sess)   
         _,_,blanket = get_blanket(blanket_resolution)
         n_batches = int(mnist.train.num_examples / ac_batch_size)
-        trainer_x, trainer_y = mnist.train.next_batch(n_label)
-
+        train_x, train_y = mnist.train.next_batch(n_label)
+        test_x, test_y = mnist.test.next_batch(n_label)
         for i in range(n_epochs_ge):
             print("------------------Epoch {}/{} ------------------".format(i, n_epochs_ge))
             for b in tqdm(range(n_batches)):    
                 #Discriminator
                 batch_x, _ = mnist.train.next_batch(ac_batch_size)
-        
-                # real batch uniform sampling for each lable. This is not constrained by lable availability.
-                dc_real_lbl = np.eye(10)[np.array(np.random.randint(0,n_leaves, size=dc_real_batch_size)).reshape(-1)]
-                dc_real_dist = gaussian_mixture(dc_real_batch_size, n_leaves,dc_real_lbl)
                 
-                # need to be sampled for each batch?
+                # random label as a selector to train z descriminator for flower graph 
+                dc_real_lbl = np.eye(10)[np.array(np.random.randint(0,n_leaves, size=dc_real_batch_size)).reshape(-1)]
+                dc_real_z = gaussian_mixture(dc_real_batch_size, n_leaves,dc_real_lbl)
                 dc_blanket_digit = np.eye(10)[np.array(np.random.randint(0,n_leaves, size=blanket_resolution*blanket_resolution)).reshape(-1)]
                 
-                sess.run([discriminator_optimizer],feed_dict={x_input: batch_x, real_distribution:dc_real_dist,\
+                sess.run([discriminator_optimizer],feed_dict={x_input: batch_x, real_distribution:dc_real_z,\
                          real_lbl:dc_real_lbl ,unif_z:blanket, unif_d:dc_blanket_digit})
+    
                 #Generator - autoencoder, fooling descriminator, and y semi-supervised classification
-                sess.run([generator_optimizer],feed_dict={x_input: batch_x,trainer_input:trainer_x, trainer_lbl:trainer_y})
+                train_xb, train_yb = next_batch(train_x,train_y,semi_sup_batch_size)
+                sess.run([generator_optimizer],feed_dict={x_input: batch_x,trainer_input:train_xb, trainer_lbl:train_yb})
                 if b % tb_log_step == 0:
                     show_discriminator(sess,1) #shows others like 3, 7 -1 ?
                     show_latent_code(sess,n_latent_sample)
